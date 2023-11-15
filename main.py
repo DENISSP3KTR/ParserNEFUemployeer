f779bb90fb799fda0c36bc4a332b7dc915354e79
Denis Osipov <54879762+DENISSP3KTR@users.noreply.github.com>
2023-11-15 17:42:43 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+# Возвращает ссылки сотрудников
+def get_staff_links(univer_name):
+    url = 'https://www.s-vfu.ru/universitet/rukovodstvo-i-struktura/instituty/'+univer_name+'/staff'
+    page = urlopen(url)
+    html = page.read().decode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
+    links = []
+    # soup.find_all('a') - список всех тегов а
+    for link in soup.find_all('a'):
+        # Получение ссылки из href лежащего внутри тега а
+        l = link.get('href')
+        # Если l не пустой и ссылка имеет /staff, то добавить в список
+        if (l is not None) and (l.startswith('/staff')):
+            links.append(l)
+    return links
+
+
+def main(univer_name):
+    staff = dict()
+    # цикл элементов из списка ссылок на сотрудников
+    for link in get_staff_links(univer_name):
+        url = 'https://www.s-vfu.ru'+link
+        page = urlopen(url)
+        html = page.read().decode('utf-8')
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Получение имени сотрудника по тегу h1, так как он единственный
+        staff_name = soup.find('h1').get_text()
+
+        # Ищим все имена категорий. Например: Должность, Ученая степень и тд
+        category_key = soup.find_all('h3', class_='h6 mb-0')
+        # Ищим все значение категорий. Например: директор, кдн
+        category_value = soup.find_all('div', class_='g-px-10 pre-wrap')
+
+        # внутри цикла создается словарь Категория ключ - имя категории, значение - значение категории
+        staff_about = dict()
+        for i, j in zip(category_key, category_value):
+            staff_about[i.text.strip()] = j.text.strip()
+
+        #создается словарь: ключ = имя сотрудника, значение = словарь Категория
+        staff[staff_name] = staff_about
+    return staff
+
+
+print(main('imi'))
+
