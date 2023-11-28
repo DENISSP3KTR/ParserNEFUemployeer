9685853ae899daa10f1f922738e6032b72ae9298
Denis Osipov <54879762+DENISSP3KTR@users.noreply.github.com>
2023-11-28 13:29:26 +0900

    hard work

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 #import pandas as pd
 
+
+
+
 # Возвращает ссылки сотрудников
 def get_staff_links(univer_name):
     url = 'https://www.s-vfu.ru/universitet/rukovodstvo-i-struktura/instituty/'+univer_name+'/staff'
@@ -19,10 +22,12 @@ def get_staff_links(univer_name):
     return links
 
 
-
 def main(univer_name):
+    staff_about = dict()
     staff = dict()
     asd = ['/staff/724792', '/staff/722703']
+    staff_category = ['Должность:', 'Повышение квалификации:', 'Участие в конференциях, симпозиумах:',
+                      'Стаж работы по специальности:']
     # цикл элементов из списка ссылок на сотрудников
     for link in asd:
         url = 'https://www.s-vfu.ru'+link
@@ -32,35 +37,43 @@ def main(univer_name):
 
         # Получение имени сотрудника по тегу h1, так как он единственный
         staff_name = soup.find('h1').get_text()
+        for i in soup.find_all('h3', class_='h6 mb-0'):
 
-        # Ищим все имена категорий. Например: Должность, Ученая степень и тд
-        category_key = soup.find_all('h3', class_='h6 mb-0')
-        # Ищим все значение категорий. Например: директор, кдн
-        category_value = soup.find_all('div', class_='g-px-10 pre-wrap')
-
-        # внутри цикла создается словарь Категория ключ - имя категории, значение - значение категории
-        staff_about = dict()
-        for i, j in zip(category_key, category_value):
-            staff_about[i.text.strip()] = j.text.strip()
-
-        #создается словарь: ключ = имя сотрудника, значение = словарь Категория
+            if i.text.strip() in staff_category:
+                catt1 = i.find_next('div')
+                casd = [tag.get_text(strip=True) for tag in catt1 if tag.get_text(strip=True)]
+                print(staff_name, casd)
+                staff_about[i.text.strip()] = casd
+                print(staff_about)
         staff[staff_name] = staff_about
+
     return staff
 
+print(main('imi')['Нюргуяна Романовна Пинигина'])
+# print(main('imi'))
 
-#print(main('imi'))
 
-def push_in_excel():
-    employees = main('imi')
-    FIO = []
-    position = []
-    training = []
-    for employee, employee_info in employees.items():
-        FIO.append(employee)
-        position.append(employee_info.get('Должность:'))
-        training.append(employee_info.get('Повышение квалификации:'))
+# def push_in_excel():
+#     employees = main('imi')
+#     FIO = []
+#     position = []
+#     training = []
+#     for employee, employee_info in employees.items():
+#         FIO.append(employee)
+#         position.append(employee_info.get('Должность:'))
+#         training.append(employee_info.get('Повышение квалификации:'))
+#     konf = []
+#     for employee, employee_info in employees.items():
+#         FIO.append(employee)
+#         position.append(employee_info.get('Повышение квалификации:'))
+#         konf.append(employee_info.get('Участие в конференциях, симпозиумах:'))
+#         print(*position)
+#         print(*konf)
+#
+#
+#
+#
+# print(training[1].split("/r"))
+# push_in_excel()
 
-    
 
-    print(training[1].split("/r"))
-push_in_excel()
